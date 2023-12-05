e0c584e7dc54586199159990e26b9c493ff2afb9
Pedro <pedro.varonaparra@usp.ceu.es>
2023-12-05 11:20:36 +0100

    mmmm

--- Practica Programacion 1/src/main/Main.java
+++ Practica Programacion 1/src/main/Main.java
@@ -1,62 +1,13 @@
 package src.main;
 
-import java.util.Scanner;
-import src.dominio.Lista;
-import src.dominio.Producto;
+import src.interfaz.Interfaz;
 
 /**
  * Clase principal que contiene el punto de entrada del programa.
  */
 public class Main {
     public static void main(String[] args) {
-        Lista listaProductos = new Lista();
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            try {
-                System.out.println("Seleccione una opción:");
-                System.out.println("1. Agregar un producto");
-                System.out.println("2. Eliminar un producto por código");
-                System.out.println("3. Ver catálogo de productos");
-                System.out.println("4. Salir");
-
-                int opcion = scanner.nextInt();
-                scanner.nextLine(); // Consumir la nueva línea después de leer un entero
-
-                switch (opcion) {
-                    case 1:
-                        System.out.print("Ingrese el nombre del modelo: ");
-                        String nombreModelo = scanner.nextLine();
-                        System.out.print("Ingrese el precio: ");
-                        double precio = scanner.nextDouble();
-                        System.out.print("Ingrese el tamaño: ");
-                        double tamano = scanner.nextDouble();
-
-                        Producto nuevoProducto = new Producto(nombreModelo, precio, tamano);
-                        listaProductos.agregarProducto(nuevoProducto);
-                        System.out.println("Producto agregado con éxito.");
-                        break;
-                    case 2:
-                        System.out.print("Ingrese el código del producto que desea eliminar: ");
-                        String codigoEliminar = scanner.next();
-                        listaProductos.eliminarProducto(codigoEliminar);
-                        System.out.println("Producto eliminado con éxito.");
-                        break;
-                    case 3:
-                        listaProductos.listarProductos();
-                        break;
-                    case 4:
-                        System.out.println("Saliendo del programa.");
-                        scanner.close();
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
-                }
-            } catch (java.util.InputMismatchException e) {
-                System.out.println("Entrada incorrecta. Por favor, ingrese una opción válida.");
-                scanner.nextLine(); // Limpiar el búfer de entrada para evitar un bucle infinito
-            }
-        }
+        Interfaz interfaz = new Interfaz(); // Crear una instancia de la clase Interfaz
+        interfaz.iniciar(); // Llamar al método iniciar de la clase Interfaz
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Practica-Progra-1
+# Catálogo de Productos
+
+Este proyecto en Java implementa un simple catálogo de productos con operaciones básicas como agregar, eliminar y mostrar productos. Utiliza una interfaz de consola para interactuar con el usuario.
+
+## Estructura del Proyecto
+
+El proyecto está organizado en tres paquetes principales:
+
+1. **src.main:** Contiene la clase principal `Main` que sirve como punto de entrada del programa.
+
+2. **src.interfaz:** Incluye la clase `Interfaz` que gestiona la interacción con el usuario y proporciona un menú para realizar diversas operaciones en el catálogo de productos.
+
+3. **src.dominio:** Contiene las clases `Producto` y `Lista`. La clase `Producto` representa un artículo en el catálogo, mientras que la clase `Lista` gestiona una colección de productos, permitiendo operaciones como agregar, eliminar y listar.
+
+## Makefile
+
+El archivo `Makefile` proporciona reglas para compilar, empaquetar, generar documentación y ejecutar el programa.
+
+- `make compile`: Compila el código fuente y crea la estructura de directorios en la carpeta `bin`.
+- `make jar`: Empaqueta el programa en un archivo JAR llamado `proyecto.jar`.
+- `make clean`: Elimina los directorios `bin` y el archivo JAR.
+- `make javadoc`: Genera la documentación Javadoc en la carpeta `doc/javadoc`.
+- `make run`: Ejecuta el programa JAR.
+
+Para realizar todas las operaciones de compilación, empaquetado, documentación y ejecución, puedes usar el comando:
+
+```bash
+make completo
+```
+
+## UML
+
+## Manifest
+
+El archivo `Manifest.txt` especifica la información necesaria para ejecutar el programa. Indica la clase principal que debe ejecutarse y define la ruta de clases con el Class-Path.
+
+Para compilar, empaquetar y ejecutar el programa, utiliza los siguientes comandos:
+
+```bash
+make completo
+```
+
+Este comando compila el código, genera la documentación Javadoc, empaqueta el programa en un archivo JAR y lo ejecuta.
